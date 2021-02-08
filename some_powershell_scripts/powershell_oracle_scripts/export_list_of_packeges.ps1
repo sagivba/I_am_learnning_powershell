5ac8f7fb0cc955f1136f62a217abec3527965f29
sagivba <30871008+sagivba@users.noreply.github.com>
2021-02-08 06:17:07 +0200

    Add files via upload

---
+++ some_powershell_scripts/powershell_oracle_scripts/export_list_of_packeges.ps1
@@ -0,0 +1,58 @@
+
+
+function Get-ExportCmd {
+	Param (
+	[string]$User,
+	[string]$Pass,
+	[string]$DB ,
+	[string]$IncludeList,
+    [string]$Directory,
+    [string]$date_str
+	)
+	# End of Parameters
+	$CMD="expdp $User/$Pass@$DB schemas=$User INCLUDE=$IncludeList SQLFILE=$FileName directory=DATA_EXP_DIR dumpfile=$User.$date_str.dmp logfile=expdp.$User.$date_str.log"
+	return $CMD
+}
+
+function Get-ImportCmd {
+	Param (
+	[string]$User,
+	[string]$Pass,
+	[string]$DB ,
+	[string]$FileName,
+    [string]$Directory,
+    [string]$date_str
+	)
+	# End of Parameters
+	$CMD="impdp $User/$Pass@$DB sqlfile=$FileName  directory=$Directory dumpfile=$User.$date_str.dmp logfile=impdp.$User.$date_str.log"
+	return $CMD
+}
+
+function Get-IncludeList {
+    Param (
+	[string]$IncludeFile
+    )
+    echo $IncludeFile
+    [array]$lines = get-content $IncludeFile
+    
+    [string]$pkg_lst =""
+    foreach ($line in $lines){
+        #PACKAGE:"IN ('DLR_PKG', 'CRM_PKG')"
+        $pkg_lst += ",\'"+$line.ToUpper()+"\'"
+    }
+    $pkg_lst=$pkg_lst.Substring(1) # trim first comma
+    $pkg_lst='PACKAGE:\"IN \(' + $pkg_lst +'\)\"'
+    echo $pkg_lst
+    return $pkg_lst
+}
+
+
+$date = get-date
+$date_str=$date.ToString("yyyy-MM-dd")
+
+$pkg_lst = Get-IncludeList ".\pckges_list.txt"
+
+Get-ExportCmd "OP" "123" "oratestc" "$pkg_lst" "DATA_EXP_DIR"  $date_str| echo
+Get-ImportCmd "OP" "123" "oratestc" "01-list.sql"  "DATA_EXP_DIR" $date_str | echo
+
+
